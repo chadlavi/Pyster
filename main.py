930476b1011f8b0c14e791d1a6dc5984b44dd3c5
Chad Lavimoniere <chad@chadlavimoniere.com>
2018-06-26 02:03:08 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,118 @@
+from flask import Flask, request, render_template, redirect
+from math import floor
+from sqlite3 import OperationalError
+import string
+import sqlite3
+try:
+    from urllib.parse import urlparse  # Python 3
+    str_encode = str.encode
+except ImportError:
+    from urlparse import urlparse  # Python 2
+    str_encode = str
+try:
+    from string import ascii_lowercase
+    from string import ascii_uppercase
+except ImportError:
+    from string import lowercase as ascii_lowercase
+    from string import uppercase as ascii_uppercase
+import base64
+
+# Assuming urls.db is in your app root folder
+app = Flask(__name__)
+ip='chdlv.pw'
+#ip='159.65.231.71'
+port='80'
+host = 'http://'+ip+'/'
+if port != '80':
+    host = 'http://'+ip+':'+port+'/'
+
+def table_check():
+    create_table = """
+        CREATE TABLE WEB_URL(
+        ID INT PRIMARY KEY AUTOINCREMENT,
+        URL TEXT NOT NULL
+        );
+        """
+    with sqlite3.connect('urls.db') as conn:
+        cursor = conn.cursor()
+        try:
+            cursor.execute(create_table)
+        except OperationalError:
+            pass
+
+
+def toBase62(num, b=62):
+    if b <= 0 or b > 62:
+        return 0
+    base = string.digits + ascii_lowercase + ascii_uppercase
+    r = num % b
+    res = base[r]
+    q = floor(num / b)
+    while q:
+        r = q % b
+        q = floor(q / b)
+        res = base[int(r)] + res
+    return res
+
+
+def toBase10(num, b=62):
+    base = string.digits + ascii_lowercase + ascii_uppercase
+    limit = len(num)
+    res = 0
+    for i in range(limit):
+        res = b * res + base.find(num[i])
+    return res
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+@app.route('/shorten', methods=['GET', 'POST'])
+def shorten():
+    if request.method == 'POST':
+        original_url = str_encode(request.form.get('url'))
+        print('original_url as input: '+original_url.decode())
+        parsed = urlparse(original_url)
+        print(parsed)
+        if not parsed.scheme:
+            print('no scheme')
+            url = 'http://' + original_url.decode('utf-8')
+        else:
+            url = original_url.decode('utf-8')
+        print('url = '+url)
+        with sqlite3.connect('urls.db') as conn:
+            cursor = conn.cursor()
+            res = cursor.execute(
+                'INSERT INTO WEB_URL (URL) VALUES (?)',
+                [base64.urlsafe_b64encode(str.encode(url))]
+            )
+            encoded_string = toBase62(res.lastrowid)
+        return render_template('shorten.html', short_url=host + encoded_string)
+    return render_template('shorten.html')
+
+
+@app.route('/<short_url>')
+def redirect_short_url(short_url):
+    decoded = toBase10(short_url)
+    print('decoded = '+str(decoded))
+    url = ''  # fallback if no URL is found
+    #url = host  # fallback if no URL is found
+    with sqlite3.connect('urls.db') as conn:
+        cursor = conn.cursor()
+        res = cursor.execute('SELECT URL FROM WEB_URL WHERE ID=?', [decoded])
+        try:
+            short = res.fetchone()
+            if short is not None:
+                url = base64.urlsafe_b64decode(short[0])
+                print('url = '+url.decode())
+        except Exception as e:
+            print(e)
+    print(url.decode())
+    return redirect(url)
+
+
+if __name__ == '__main__':
+    # This code checks whether database table is created or not
+    table_check()
+    app.run(host=ip, port=port)
