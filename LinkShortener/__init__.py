27604179496f1f9dcd13ec08c5eee347bdd1e80a
Chad Lavimoniere <chad@chadlavimoniere.com>
2018-06-27 03:42:50 +0000

    now it works tho

--- LinkShortener/__init__.py
+++ LinkShortener/__init__.py
@@ -20,6 +20,8 @@ import base64
 # Assuming urls.db is in your app root folder
 app = Flask(__name__, static_url_path='/static/')
 
+host='http://chdlv.pw/'
+
 def table_check():
     create_table = """
         CREATE TABLE WEB_URL(
@@ -27,7 +29,7 @@ def table_check():
         URL TEXT NOT NULL
         );
         """
-    with sqlite3.connect('urls.db') as conn:
+    with sqlite3.connect('/var/www/LinkShortener/LinkShortener/urls.db') as conn:
         cursor = conn.cursor()
         try:
             cursor.execute(create_table)
@@ -56,7 +58,6 @@ def toBase10(num, b=62):
         res = b * res + base.find(num[i])
     return res
 
-
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('home.html')
@@ -74,11 +75,11 @@ def shorten():
         else:
             url = original_url.decode('utf-8')
         #print('url = '+url)
-        with sqlite3.connect('urls.db') as conn:
+        with sqlite3.connect('/var/www/LinkShortener/LinkShortener/urls.db') as conn:
             cursor = conn.cursor()
             res = cursor.execute(
                 'INSERT INTO WEB_URL (URL) VALUES (?)',
-                [base64.urlsafe_b64encode(str.encode(url))]
+                [base64.urlsafe_b64encode(url)]
             )
             encoded_string = toBase62(res.lastrowid)
         return render_template('shorten.html', short_url=host + 'i/' +  encoded_string)
@@ -92,23 +93,24 @@ def static_from_root():
 @app.route('/i/<short_url>')
 def redirect_short_url(short_url):
     decoded = toBase10(short_url)
-    #print('decoded = '+str(decoded))
+    print('decoded = '+str(decoded))
     url = ''  # fallback if no URL is found
-    with sqlite3.connect('urls.db') as conn:
+    with sqlite3.connect('/var/www/LinkShortener/LinkShortener/urls.db') as conn:
         cursor = conn.cursor()
         res = cursor.execute('SELECT URL FROM WEB_URL WHERE ID=?', [decoded])
         try:
             short = res.fetchone()
             if short is not None:
-                url = base64.urlsafe_b64decode(short[0])
-                #print('url = '+url)
+                print('short[0] = ' + short[0])
+                url = base64.urlsafe_b64decode(short[0].encode(encoding='UTF-8'))
+                print('url = '+url)
         except Exception as e:
             print(e)
-    #print(url)
+    print('url = '+url)
     return redirect(url)
 
 
 if __name__ == '__main__':
     # This code checks whether database table is created or not
-    table_check()
-    app.run(host=ip, port=port)
+    #table_check()
+    app.run()
